9deb29e3803ad49804c32e47986e9da0c19cfdb4
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-14 03:11:35 +0500

    update files

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => (
-  <nav className="navBar">
-    <div className="navItems">
+const Nav = () => (
+  <nav className="nav-bar">
+    <div className="nav-items">
       <h2>Bookstore CMS</h2>
-      <ul className="navLinks">
+      <ul className="nav-links">
         <li><Link to="/">Books</Link></li>
-        <li><Link to="categories">Categories</Link></li>
+        <li><Link to="catagory">Catagories</Link></li>
       </ul>
     </div>
     <img src="https://img.icons8.com/external-anggara-outline-color-anggara-putra/32/000000/external-avatar-interface-anggara-outline-color-anggara-putra.png" alt="profile" />
   </nav>
 );
 
-export default Navbar;
+export default Nav;
