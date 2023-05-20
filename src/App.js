84fa4ad2afcf30e404711041861966851795ae21
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-21 04:00:31 +0500

    update app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Books from './components/Books';
-import Categories from "./components/Categories.js";
+import Categories from './components/Categories.js';
 
 function App() {
   return (
