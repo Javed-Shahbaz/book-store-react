dfcde1cd977c51abc0f8206e92377e5593de5fe7
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-21 03:53:15 +0500

    update files

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Books />} />
-        <Route exact path="Categories" element={<Categories />} />
+        <Route exact path="categories" element={<Categories />} />
       </Routes>
 
     </div>
