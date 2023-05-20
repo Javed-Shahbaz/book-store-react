8bde1a7688712d0e8de56b4e2a2bfcab4f7e1c8b
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-20 20:32:58 +0500

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import bookReducer from './books/booksSlice';
-import categoryReducer from './categories/categoriesSlice';
+import bookReducer from './books/bookSlice';
+import categoryReducer from './categories/categoriesSlice.js';
 
 const store = configureStore({
   reducer: {
@@ -8,5 +8,3 @@ const store = configureStore({
     category: categoryReducer,
   },
 });
-
-export default store;
